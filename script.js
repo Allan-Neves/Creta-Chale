6a6a272da4c3bf77796b4be2199eb2f99003aa7d
Allan Carlos <allan3017carlos@gmail.com>
2023-10-09 16:48:33 -0300

    Adicionando menu hamburguer

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // ============ Configuração do slider ============ \\
-
 let slideIndex = 0;
 let slides = document.getElementsByClassName("mySlides");
 let i;
@@ -38,3 +37,60 @@ function plusSlides(n) {
 
   slides[slideIndex - 1].style.display = "block";
 }
+
+// ============ Configuração do menu hambúrguer ============ \\
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li"
+);
+mobileNavbar.init();
+
+// ============ Configuração da navegação suave ============ \\
+var links = document.querySelectorAll("#menu a");
+
+links.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    var section = document.querySelector(link.getAttribute("href"));
+    section.scrollIntoView({ behavior: "smooth" });
+  });
+});
